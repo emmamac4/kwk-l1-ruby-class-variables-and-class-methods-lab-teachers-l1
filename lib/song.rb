bbdcd19d26d5bcf6d5bcbbe866829dccec010a95
Emma Butler <emmamac4@gmail.com>
2018-06-25 14:49:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,30 @@
+class Song
 
+  attr_accessor :name, :artist, :genre
+  @@count=0
+  @@artists=[]
+  @@genre=[]
+
+
+  def initialize
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count+=1
+    @@genre << genre
+    @@artists << artists
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genre
+    @@genre=genre
+  end
+
+end
